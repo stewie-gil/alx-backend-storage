4c3ef238a533ca130dd819304f897a13e9f58d6d
stewie-gil <gilliganstewie7@gmail.com>
2023-08-16 19:02:42 -0700

    m

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,22 +4,30 @@
 import redis
 import uuid
 from functools import wraps
-from typing import Callable, Optional
+from typing import Callable
 
+def count_calls(method: Callable) -> Callable:
+    """Decorator that counts method calls"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 class Cache:
-    """ The cache class"""
+    """The cache class"""
 
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None):
+    def get(self, key: str, fn: Callable = None):
         value = self._redis.get(key)
         if value is None:
             return None
@@ -33,16 +41,3 @@ class Cache:
     def get_int(self, key: str):
         return self.get(key, fn=int)
 
-
-def count_calls(method: Callable) -> Callable:
-    @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        method_name = method.__qualname__
-        self._redis.incr(method_name)
-        return method(self, *args, **kwargs)
-    return wrapper
-
-
-Cache.store = count_calls(Cache.store)
-
-cache = Cache()
